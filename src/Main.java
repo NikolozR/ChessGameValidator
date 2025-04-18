bf19c3f76914a2bdcf072e8d99482b04ae92dd3a
NikolozR <nika.rusishvili.95@gmail.com>
2025-04-19 00:48:56 +0400

    making the project usable

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,11 @@
-import game.GameEntry;
-import game.GameMove;
-import game.Simulation;
-import io.PGNReader;
-import io.Tokenizer;
+import io.ChessValidator;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        PGNReader pgnReader = new PGNReader();
-        pgnReader.extractGames("src/io/test.pgn");
-        Tokenizer tokenizer = new Tokenizer();
-        Simulation simulation = new Simulation();
-        for (GameEntry g : pgnReader.getGames()) {
-            tokenizer.tokenize(g.getGameNumber(), g.getGameMoves());
-        }
-        for (GameEntry g : pgnReader.getGames()) {
-            System.out.println("Analyzing game N" + g.getGameNumber());
-            int errorCount = 0;
-            ArrayList<GameMove> currentGame = tokenizer.getTokenizedGameHistory().get(g.getGameNumber());
-            for (GameMove gm : currentGame) {
-                if (gm.isValidLength()) {
-                    if (!gm.getWhitesMove().isValidMove()) {
-                        errorCount++;
-                        System.out.println(gm.getWhitesMove().getErrorStatusText());
-                    }
-                    if (!gm.getBlacksMove().isValidMove()) {
-                        errorCount++;
-                        System.out.println(gm.getBlacksMove().getErrorStatusText());
-                    }
-                } else {
-                    errorCount++;
-                    System.out.println(gm.getErrorStatusText());
-                }
-            }
-            if (errorCount == 0) {
-                simulation.addValidGameHistories(g.getGameNumber(), currentGame);
-                System.out.println("Game had No Errors, Valid Syntax");
-            } else {
-                System.out.println("Game had Errors!");
-            }
-            System.out.println();
-            System.out.println("-----------------------------------------------------------------------------------");
-            System.out.println();
-        }
-        simulation.simulate();
-
-
-//        for (game.GameEntry g : pgnReader.getGames()) {
-//            System.out.println("Game #" + g.getGameNumber());
-//            System.out.println("Event: " + g.getEvent());
-//            System.out.println("Date: " + g.getDate());
-//            System.out.println("White: " + g.getWhite());
-//            System.out.println("Black: " + g.getBlack());
-//            System.out.println("Moves: " + g.getGameMoves());
-//            System.out.println("---------------------------------------------------");
-//        }
+        String path = args.length > 0 ? args[0] : "src/io/test.pgn";
+        ChessValidator.run(path);
     }
 }
+
